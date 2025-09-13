434d599e2dbfbb798458de9b7cddc2f389e943f6
Matthew Maurer <maurer.it@gmail.com>
2025-09-13 05:34:17 -0400

    checkstyle issue

--- backend/src/main/java/com/namedraw/service/UserService.java
+++ backend/src/main/java/com/namedraw/service/UserService.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
